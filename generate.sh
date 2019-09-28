7716226405e591d6c320dd5282f7cdf0b61a3a7f
Serger <gennaro.raiola@gmail.com>
2019-09-27 17:13:18 -0700

    Fix

--- generate.sh
+++ generate.sh
@@ -29,6 +29,6 @@ do
 done
 
 # Finally commit and push the generated files
-git add $CV_NAME.pdf $MD_NAME $FOLDER_NAME
+git add $CV_NAME.pdf $CV_NAME.tex $MD_NAME $FOLDER_NAME
 git commit -m "Generated files updated on $CURR_DATE"
 git push
